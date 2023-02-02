5c41cbde948907f27e7d9ce2e8b8e0fea12ee495
Titus Williams <wtwilliams310@gmail.com>
2023-02-02 12:28:47 -0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+(async () => {
+    // This is the entry point for your application. Write all of your code here.
+    // Before you can use the database, you need to configure the "db" object 
+    // with your team name in the "js/movies-api.js" file.
+    
+    
+})();
