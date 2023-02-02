bfbba400352d42e7fe325e657363f4adcc9ce04c
Andre Murray <slushymatis@gmail.com>
2023-02-02 14:54:14 -0600

    des: pull movie info from api to html

--- js/index.js
+++ js/index.js
@@ -3,5 +3,22 @@
     // Before you can use the database, you need to configure the "db" object 
     // with your team name in the "js/movies-api.js" file.
     
-    
+    getMovies().then(movies =>  {
+        let moviez = document.querySelector("#movie");
+        console.log(movies)
+        let html = "";
+        for(let i = 0; i < movies.length; i++) {
+            html += ` <div>
+                        <h1>${movies[i].title}</h1>
+                        <div>${movies[i].actors} Actors</div>
+                        <div>${movies[i].director} Director</div>
+                        <div>${movies[i].rating} Rating</div>
+                        <div>${movies[i].runtime} Runtime</div>
+                        <div>${movies[i].year} Year</div>
+                        
+                        
+                    </div>`
+        }
+        moviez.innerHTML = html;
+    })
 })();
